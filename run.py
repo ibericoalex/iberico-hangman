626caa31733dbe6e26c82f92565dc1de5a3f8291
Your Name <you@example.com>
2023-09-13 22:23:20 +0200

    Add function to select game difficulty.

--- run.py
+++ run.py
@@ -30,3 +30,13 @@ def clear_screen():
 def get_word(difficulty):
     word_length_config = LETTER_COUNT_BY_DIFFICULTY[difficulty]
     return random.choice([word for word in word_list if word_length_config['min'] <= len(word) <= word_length_config['max']])
+
+def get_difficulty(username):
+    print(f"\nHello, {username}! Choose a difficulty level:")
+    print("1. Easy")
+    print("2. Medium")
+    print("3. Hard")
+    difficulty = input("Enter a number (1/2/3): ")
+    while difficulty not in ["1", "2", "3"]:
+        difficulty = input(f"Invalid choice, {username}. Please choose again (1/2/3): ")
+    return difficulty
