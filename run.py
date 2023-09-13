749907c10bb206e3d87891e6e049a4a654779478
Your Name <you@example.com>
2023-09-13 22:17:33 +0200

    Define constants for game difficulty settings.

--- run.py
+++ run.py
@@ -2,3 +2,24 @@ import os
 import random
 from words import word_list
 from hangman_pics import HANGMAN_PICS
+
+MAX_GUESSES_BY_DIFFICULTY = {
+    "1": 5,
+    "2": 6,
+    "3": 8
+}
+
+LETTER_COUNT_BY_DIFFICULTY = {
+    "1": {
+        'min': 3,
+        'max': 3
+    },
+    "2": {
+        'min': 4,
+        'max': 6
+    },
+    "3":{
+        'min': 7,
+        'max': 150
+    },
+}
